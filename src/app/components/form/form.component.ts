5e46f4199a0400b1bf1acb1d7b33afca5726b231
Kaisar <kaisar.nassipolla@gmail.com>
2024-11-08 23:18:52 +0500

    created 2 functions for delete and add orders

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,33 +1,47 @@
-import { Component } from '@angular/core';
-import {MatFormField} from "@angular/material/form-field";
+import {Component} from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
-import {MatInput} from "@angular/material/input";
-import {MatButton} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {
+  MatTableDataSource,
+  MatTableModule
+} from "@angular/material/table";
+import {CommonModule, NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [
-    MatFormField,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    NgFor,
     FormsModule,
-    MatInput,
-    MatButton
+    CommonModule
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-  formData = {
-    name: '',
-    surname: ''
+  formData = { orderName: '', quantity: null, unitPrice: null };
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['orderName', 'quantity', 'unitPrice', 'totalPrice', 'actions'];
+
+  addOrder() {
+    if (this.formData.orderName && this.formData.quantity && this.formData.unitPrice) {
+      this.dataSource.data = [...this.dataSource.data, { ...this.formData }];
+      this.formData = { orderName: '', quantity: null, unitPrice: null };
+    }
   };
 
-  dataSource: any[] = [];
-
-  addData() {
-    if (this.formData.name && this.formData.surname) {
-      this.dataSource.push({ ...this.formData });
-      this.formData = { name: '', surname: '' };
+  removeOrder(order: any) {
+    const data = this.dataSource.data;
+    const index = data.indexOf(order);
+    if (index > -1) {
+      data.splice(index, 1);
+      this.dataSource.data = [...data];
     }
   };
 }
